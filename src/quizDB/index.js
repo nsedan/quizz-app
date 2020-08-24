d70a56d24147f0c4eb7c9fe351e6d32d9250c0c0
nsedan <naim.alejandro.sedan@gmail.com>
2020-08-24 23:20:11 +0000

    uuid API added to questionsDB

--- src/quizDB/index.js
+++ src/quizDB/index.js
@@ -1,24 +1,25 @@
+const { v4: uuidv4 } = require('uuid');
 const questionsDB = [
   {
     question:
       "Virgin Trains, Virgin Atlantic and Virgin Racing, are all companies owned by which famous entrepreneur?   ",
     answers: ["Richard Branson", "Alan Sugar", "Donald Trump", "Bill Gates"],
     correct: "Richard Branson",
-    questionId: "099099"
+    questionId: uuidv4()
   },
   {
     question:
       'Where is the train station "Llanfair­pwllgwyngyll­gogery­chwyrn­drobwll­llan­tysilio­gogo­goch"?',
     answers: ["Wales", "Moldova", "Czech Republic", "Denmark"],
     correct: "Wales",
-    questionId: "183452"
+    questionId: uuidv4()
   },
   {
     question:
       "Which company did Valve cooperate with in the creation of the Vive?",
     answers: ["HTC", "Oculus", "Google", "Razer"],
     correct: "HTC",
-    questionId: "267908"
+    questionId: uuidv4()
   },
   {
     question: "What's the name of Batman's  parents?",
@@ -29,13 +30,13 @@ const questionsDB = [
       "Todd & Mira"
     ],
     correct: "Thomas & Martha",
-    questionId: "333247"
+    questionId: uuidv4()
   },
   {
     question: "What is the most common surname Wales?",
     answers: ["Jones", "Williams", "Davies", "Evans"],
     correct: "Jones",
-    questionId: "496293"
+    questionId: uuidv4()
   },
   {
     question:
@@ -47,27 +48,27 @@ const questionsDB = [
       "The British Bulldogs"
     ],
     correct: "Demolition",
-    questionId: "588909"
+    questionId: uuidv4()
   },
   {
     question:
       'What name represents the letter "M" in the NATO phonetic alphabet?',
     answers: ["Mike", "Matthew", "Mark", "Max"],
     correct: "Mike",
-    questionId: "648452"
+    questionId: uuidv4()
   },
   {
     question: "What is the first book of the Old Testament?",
     answers: ["Genesis", "Exodus", "Leviticus", "Numbers"],
     correct: "Genesis",
-    questionId: "786649"
+    questionId: uuidv4()
   },
   {
     question:
       "In the video-game franchise Kingdom Hearts, the main protagonist, carries a weapon with what shape?",
     answers: ["Key", "Sword", "Pen", "Cellphone"],
     correct: "Key",
-    questionId: "839754"
+    questionId: uuidv4()
   },
   {
     question:
@@ -79,63 +80,63 @@ const questionsDB = [
       "Rubik’s Cube"
     ],
     correct: "Cabbage Patch Kids",
-    questionId: "98390"
+    questionId: uuidv4()
   },
   {
     question: "What does a funambulist walk on?",
     answers: ["A Tight Rope", "Broken Glass", "Balls", "The Moon"],
     correct: "A Tight Rope",
-    questionId: "1071006"
+    questionId: uuidv4()
   },
   {
     question: "In past times, what would a gentleman keep in his fob pocket?",
     answers: ["Watch", "Money", "Keys", "Notebook"],
     correct: "Watch",
-    questionId: "1174154"
+    questionId: uuidv4()
   },
   {
     question: "Area 51 is located in which US state?",
     answers: ["Nevada", "Arizona", "New Mexico", "Utah"],
     correct: "Nevada",
-    questionId: "1226535"
+    questionId: uuidv4()
   },
   {
     question: "How would one say goodbye in Spanish?",
     answers: ["Adiós", " Hola", "Au Revoir", "Salir"],
     correct: "Adiós",
-    questionId: "1310938"
+    questionId: uuidv4()
   },
   {
     question: "What is the largest organ of the human body?",
     answers: ["Skin", "Heart", "large Intestine", "Liver"],
     correct: "Skin",
-    questionId: "1436873"
+    questionId: uuidv4()
   },
   {
     question: "Which sign of the zodiac is represented by the Crab?",
     answers: ["Cancer", "Libra", "Virgo", "Sagittarius"],
     correct: "Cancer",
-    questionId: "1515110"
+    questionId: uuidv4()
   },
   {
     question: "On a dartboard, what number is directly opposite No. 1?",
     answers: ["19", "20", "12", "15"],
     correct: "19",
-    questionId: "1642728"
+    questionId: uuidv4()
   },
   {
     question:
       "What does the 'S' stand for in the abbreviation SIM, as in SIM card? ",
     answers: ["Subscriber", "Single", "Secure", "Solid"],
     correct: "Subscriber",
-    questionId: "1747256"
+    questionId: uuidv4()
   },
   {
     question:
       "What word represents the letter 'T' in the NATO phonetic alphabet?",
     answers: ["Tango", "Target", "Taxi", "Turkey"],
     correct: "Tango",
-    questionId: "1822532"
+    questionId: uuidv4()
   },
   {
     question: "Which American president appears on a one dollar bill?",
@@ -146,19 +147,19 @@ const questionsDB = [
       "Benjamin Franklin"
     ],
     correct: "George Washington",
-    questionId: "195075"
+    questionId: uuidv4()
   },
   {
     question: 'What is "dabbing"?',
     answers: ["A dance", "A medical procedure", "A sport", "A language"],
     correct: "A dance",
-    questionId: "2019778"
+    questionId: uuidv4()
   },
   {
     question: "What is the name of the Jewish New Year?",
     answers: ["Rosh Hashanah", "Elul", "New Year", "Succoss"],
     correct: "Rosh Hashanah",
-    questionId: "2134343"
+    questionId: uuidv4()
   },
   {
     question: "Which one of the following rhythm games was made by Harmonix?",
@@ -169,26 +170,26 @@ const questionsDB = [
       "Dance Dance Revolution"
     ],
     correct: "Rock Band",
-    questionId: "2210799"
+    questionId: uuidv4()
   },
   {
     question:
       "What type of animal was Harambe, who was shot after a child fell into it's enclosure at the Cincinnati Zoo?",
     answers: ["Gorilla", "Tiger", "Panda", "Crocodile"],
     correct: "Gorilla",
-    questionId: "2379831"
+    questionId: uuidv4()
   },
   {
     question: "Red Vines is a brand of what type of candy?",
     answers: ["Licorice", "Lollipop", "Chocolate", "Bubblegum"],
     correct: "Licorice",
-    questionId: "2426418"
+    questionId: uuidv4()
   },
   {
     question: "What is the nickname of the US state of California?",
     answers: ["Golden State", "Sunshine State", "Bay State", "Treasure State"],
     correct: "Golden State",
-    questionId: "2510086"
+    questionId: uuidv4()
   },
   {
     question: "What is on display in the Madame Tussaud's museum in London?",
@@ -199,32 +200,32 @@ const questionsDB = [
       "Vintage cars"
     ],
     correct: "Wax sculptures",
-    questionId: "2685745"
+    questionId: uuidv4()
   },
   {
     question:
       "What was the nickname given to the Hughes H-4 Hercules, a heavy transport flying boat which achieved flight in 1947?",
     answers: ["Spruce Goose", "Noah's Ark", "Fat Man", "Trojan Horse"],
     correct: "Spruce Goose",
-    questionId: "2796884"
+    questionId: uuidv4()
   },
   {
     question: "Which of these colours is NOT featured in the logo for Google?",
     answers: ["Pink", "Yellow", "Blue", "Green"],
     correct: "Pink",
-    questionId: "2838900"
+    questionId: uuidv4()
   },
   {
     question: 'What is the French word for "hat"?',
     answers: ["Chapeau", "Bonnet", " Écharpe", " Casque"],
     correct: "Chapeau",
-    questionId: "298060"
+    questionId: uuidv4()
   },
   {
     question: "Five dollars is worth how many nickles?",
     answers: ["100", "50", "25", "69"],
     correct: "100",
-    questionId: "3096579"
+    questionId: uuidv4()
   },
   {
     question: "Who is depicted on the US hundred dollar bill?",
@@ -235,7 +236,7 @@ const questionsDB = [
       "Thomas Jefferson"
     ],
     correct: "Benjamin Franklin",
-    questionId: "3182461"
+    questionId: uuidv4()
   },
   {
     question: "What do the letters in the GMT time zone stand for?",
@@ -246,32 +247,32 @@ const questionsDB = [
       "Glasgow Man Time"
     ],
     correct: "Greenwich Mean Time",
-    questionId: "3239112"
+    questionId: uuidv4()
   },
   {
     question: "Which one of these is not a typical European sword design?",
     answers: ["Scimitar", "Falchion", "Ulfberht", "Flamberge"],
     correct: "Scimitar",
-    questionId: "3318503"
+    questionId: uuidv4()
   },
   {
     question:
       'According to Sherlock Holmes, "If you eliminate the impossible, whatever remains, however improbable, must be the..."',
     answers: ["Truth", "Answer", "Cause", "Source"],
     correct: "Truth",
-    questionId: "3410327"
+    questionId: uuidv4()
   },
   {
     question: "What is the name of Poland in Polish?",
     answers: ["Polska", "Pupcia", "Polszka", "Póland"],
     correct: "Polska",
-    questionId: "3542596"
+    questionId: uuidv4()
   },
   {
     question: "The New York Times slogan is, “All the News That’s Fit to…”",
     answers: ["Print", "Digest", "Look", "Read"],
     correct: "Print",
-    questionId: "3667517"
+    questionId: uuidv4()
   },
   {
     question: "What do the letters of the fast food chain KFC stand for?",
@@ -282,45 +283,45 @@ const questionsDB = [
       "Kiwi Food Cut"
     ],
     correct: "Kentucky Fried Chicken",
-    questionId: "3791672"
+    questionId: uuidv4()
   },
   {
     question: "Which restaurant's mascot is a clown?",
     answers: ["McDonald's", "Whataburger", "Burger King", "Sonic"],
     correct: "McDonald's",
-    questionId: "3893585"
+    questionId: uuidv4()
   },
   {
     question: 'What color is the "Ex" in FedEx Ground?',
     answers: ["Green", "Red", "Light Blue", "Orange"],
     correct: "Green",
-    questionId: "3913430"
+    questionId: uuidv4()
   },
   {
     question: "How tall is the Burj Khalifa?",
     answers: ["2,722 ft", "2,717 ft", "2,546 ft", "3,024 ft"],
     correct: "2,722 ft",
-    questionId: "4049121"
+    questionId: uuidv4()
   },
   {
     question:
       "Which of the following card games revolves around numbers and basic math?",
     answers: ["Uno", "Go Fish", "Twister", "Munchkin"],
     correct: "Uno",
-    questionId: "4150746"
+    questionId: uuidv4()
   },
   {
     question:
       "What machine element is located in the center of fidget spinners?",
     answers: ["Bearings", "Axles", "Gears", "Belts"],
     correct: "Bearings",
-    questionId: "4235063"
+    questionId: uuidv4()
   },
   {
     question: "Which sign of the zodiac comes between Virgo and Scorpio?",
     answers: ["Libra", "Gemini", "Taurus", "Capricorn"],
     correct: "Libra",
-    questionId: "4321002"
+    questionId: uuidv4()
   },
   {
     question: "Which of the following presidents is not on Mount Rushmore?",
@@ -331,7 +332,7 @@ const questionsDB = [
       "Thomas Jefferson"
     ],
     correct: "John F. Kennedy",
-    questionId: "4442286"
+    questionId: uuidv4()
   },
   {
     question: "What is Tasmania?",
@@ -342,32 +343,32 @@ const questionsDB = [
       "The Name of a Warner Brothers Cartoon Character"
     ],
     correct: "An Australian State",
-    questionId: "4564430"
+    questionId: uuidv4()
   },
   {
     question: "What company developed the vocaloid Hatsune Miku?",
     answers: ["Crypton Future Media", "Sega", "Sony", "Yamaha Corporation"],
     correct: "Crypton Future Media",
-    questionId: "4630606"
+    questionId: uuidv4()
   },
   {
     question:
       "Which country, not including Japan, has the most people of japanese decent?",
     answers: ["Brazil", "China", "South Korea", "United States of America"],
     correct: "Brazil",
-    questionId: "4795960"
+    questionId: uuidv4()
   },
   {
     question: "Which candy is NOT made by Mars?",
     answers: ["Almond Joy", "M&M's", "Twix", "Snickers"],
     correct: "Almond Joy",
-    questionId: "4811162"
+    questionId: uuidv4()
   },
   {
     question: "In which fast food chain can you order a Jamocha Shake?",
     answers: ["Arby's", "McDonald's", "Burger King", "Wendy's"],
     correct: "Arby's",
-    questionId: "4982981"
+    questionId: uuidv4()
   }
 ];
 
