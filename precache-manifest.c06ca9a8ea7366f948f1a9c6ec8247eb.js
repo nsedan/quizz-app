64d9f346b7afa6686e57679f7d84199abd56a6fe
nsedan <naim.alejandro.sedan@gmail.com>
2020-08-25 00:43:03 +0000

    Updates

---
+++ precache-manifest.c06ca9a8ea7366f948f1a9c6ec8247eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd65ef09a44c632241f48fc756bd76e",
+    "url": "/quizzer-app/index.html"
+  },
+  {
+    "revision": "355cbd784776565f7e74",
+    "url": "/quizzer-app/static/css/main.f760a3fa.chunk.css"
+  },
+  {
+    "revision": "bfdbe3fe55f0b556c60c",
+    "url": "/quizzer-app/static/js/2.518df9da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizzer-app/static/js/2.518df9da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "355cbd784776565f7e74",
+    "url": "/quizzer-app/static/js/main.774c9655.chunk.js"
+  },
+  {
+    "revision": "488e55b42a1dfd55d4ea",
+    "url": "/quizzer-app/static/js/runtime-main.f55794ab.js"
+  }
+]);
